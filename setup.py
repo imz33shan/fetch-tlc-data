3835293042937b306ef0148959e35f8229c7c46e
Zeeshan Atta <zeeshanatta17@gmail.com>
2024-01-10 17:39:42 +0500

    Updated install_requires in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'requests=2.31.0',
-        'pandas=2.1.4',
-        'beautifulsoup4=4.12.2',
-        'pyarrow=14.0.2',
-        'lxml=5.1.0',
-        'openpyxl=3.1.2'
+        'requests',
+        'pandas',
+        'beautifulsoup4',
+        'pyarrow',
+        'lxml',
+        'openpyxl'
     ],
 )
